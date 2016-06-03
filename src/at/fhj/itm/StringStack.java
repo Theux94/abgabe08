5e6577c2ad18b44a2df6a4984a963b25664d5ed8
devA <wichtigms@posteo.de>
2016-06-03 11:34:31 +0200

    deleted comments

--- src/at/fhj/itm/StringStack.java
+++ src/at/fhj/itm/StringStack.java
@@ -1,5 +1,70 @@
 package at.fhj.itm;
 
-public class StringStack {
+import at.fhj.itm.api.Stack;
+
+
+
+
+public class StringStack implements Stack
+{
+	private String[] stack;
+	private int counter;
+
+
+	public StringStack(int stacklength) {
+		this.stack = new String[stacklength];
+		this.counter = 0;
+	}
+
+	
+	
+	@Override
+	public boolean isEmpty() {
+		if(counter == 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	
+	@Override
+	public void push(String item) {
+		if(counter < stack.length){
+			stack[counter] = item;
+			counter++;
+		}else{
+			System.out.println("Stack is full!");
+		}
+	}
+
+	
+	@Override
+	public String pop() {
+		if(isEmpty()){
+			return "Stack is empty!";
+		}else{
+			counter--;
+			String cache = stack[counter];
+			stack[counter] = null;
+			return cache;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		StringBuilder stackstring = new StringBuilder();
+		for(int i = 0; i<stack.length; i++){
+			if(stack[i] != null){
+				stackstring.append(stack[i] + " ");
+			}else{
+				break;
+			}
+			
+		}
+		return stackstring.toString();
+	}
 
 }
